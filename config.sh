2b54aea96c5e57fc04a11b69fb284884ec49d1e3
Daniel Roudnitsky <droudnitsky3921@bths.edu>
2016-08-02 12:40:01 -0400

    gensim run_tests

--- config.sh
+++ config.sh
@@ -10,6 +10,6 @@ function pre_build {
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    pip install astropy
-    python -c 'import sys; import astropy; sys.exit(astropy.test())'
+    pip install gensim
+    python -c 'import sys; import gensim; sys.exit(gensim.test())'
 }
